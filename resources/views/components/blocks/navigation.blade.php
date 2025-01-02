4f638cdc7be7f0f149145261ae8fd61a44f2487f
Dew <falsedew@internet.ru>
2025-01-02 06:52:52 +0300

    Инициализация проекта

---
+++ resources/views/components/blocks/navigation.blade.php
@@ -0,0 +1,20 @@
+<div class="grid grid-cols-3 items-center py-4">
+    <div>
+        <h1 class="text-xl font-bold">ДАРИТЬ ЛЕГКО</h1>
+        сервис подарков
+    </div>
+    <nav>
+        <ul class="flex space-x-4">
+            <li>IPhone</li>
+            <li>AirPods</li>
+            <li>Watch</li>
+            <li>IPad</li>
+            <li>Mac</li>
+            <li>Dyson</li>
+        </ul>
+    </nav>
+    <div class="flex justify-end">
+        <a>8-930-600-96-20</a>
+        <x-signs.whats-app />
+    </div>
+</div>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>ДАРИТЬ ЛЕГКО - сервис подарков</title>
+</head>
+
+<body>
+    <header class="max-w-7xl mx-auto px-4">
+        <x-blocks.navigation />
+    </header>
+    <main class="max-w-7xl mx-auto px-4">
+        <div class="h-64 bg-gray-100"></div>
+        <p class="text-center">
+            Ничто не сравнится с теплыми чувствами, которые наполняют нас от общения с близкими людьми,<br>
+            с которыми мы можем поделиться всем. Выразить нашу заботу, внимание, любовь - это мощнейший способ<br>
+            почувствовать внутреннюю теплоту и гармонию. Это может быть слово, дело, поступок. Совсем не обязательно<br>
+            физический подарок – но и его можно превратить в нечто большее. Наша миссия – помочь вам в этом. <b>Дарите легко.</b>
+        </p>
+        <span>КАК ПОРАДОВАТЬ БЛИЗКИХ</span>
+        <div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <span>КАК ПОРАДОВАТЬ БЛИЗКИХ</span>
+        <div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <span>Окунитесь в тёплые эмоции радости и счастья.  Дарите легко</span>
+        <div>
+            Остались вопросы - напишите нам Whats'App или позвоните <a>8-930-600-96-20</a>
+        </div>
+    </main>
+    <footer class="max-w-7xl mx-auto p-4">
+        <x-blocks.navigation />
+        <div>
+            Сайт носит сугубо информационный характер и не является публичной офертой, определяемой Статьей 437 (2) ГК
+            РФ. Apple и логотип Apple являются
+            зарегистрированными товарными знаками компании Apple Inc. в США и других странах. App Store является знаком
+            обслуживания компании Apple Inc.
+        </div>
+        <div>
+            ИП Маланина Н.Б., ИНН 502711313209
+        </div>
+    </footer>
+</body>
+
+</html>
